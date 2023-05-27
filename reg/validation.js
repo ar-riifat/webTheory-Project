6adc53a4bc29df5eb278a5c183023acd2998fd03
ar-riifat <arrifat40@gmail.com>
2023-05-28 03:54:08 +0600

    adding php

--- reg/validation.js
+++ reg/validation.js
@@ -1,9 +1,13 @@
 function formValidation() {
     var fname = document.getElementById("ifname").value;
     var lname = document.getElementById("ilname").value;
-    var ipass = document.getElementById("ipass").value;
+    var pass = document.getElementById("ipass").value;
+    var mobile = document.getElementById("imob").value;
+    var email = document.getElementById("iemail").value;
     var namePattern = /^[a-zA-Z._]+$/
-    var passPattern = /((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*><+-_=/,.])){8,40}/
+    var passPattern = /((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*><+-_=/,.])).{8,40}/
+    var mobPattern = /(\+88)?-?01[3-9]\d{8}/
+    var mailPattern = /(cse|eee|law|bs|is)_\d{10}@lus\.ac\.bd/
 
     if (fname.length < 3 || fname.length > 20) {
         document.getElementById("efname").innerHTML = "**length 3-20";
@@ -24,11 +28,29 @@ function formValidation() {
     } else {
         document.getElementById("elname").innerHTML = "";
     }
-   if (ipass.match(passPattern)) {
-        document.getElementById("epass").innerHTML = "1 Upper 1 Lower 1 Digit 1 Special Character Length 8-40!!!";
+   if (!pass.match(passPattern)) {
+        document.getElementById("epass").innerHTML = "**1 Upper 1 Lower, 1 Digit, 1 SC, Length 8-40!!!";
         return false;
 }
-
+else    if (pass.match(passPattern)) {
+    document.getElementById("epass").innerHTML = "";
+}
+//mobile
+if (!mobile.match(mobPattern)) {
+    document.getElementById("emob").innerHTML = "**Only BD Number Is Allowed!!!";
+    return false;
+}
+else    if (mobile.match(mobPattern)) {
+document.getElementById("emob").innerHTML = "";
+}
+//mail
+if (!email.match(mailPattern)) {
+    document.getElementById("eemail").innerHTML = "**Only LU Mail Is Allowed!!!";
+    return false;
+}
+else    if (email.match(mailPattern)) {
+document.getElementById("eemail").innerHTML = "";
+}
 
 
 }
